78976b89d43b2b66e2d51ced27709acd8b97c5a2
RezaKarami70 <reza.karami70@gmail.com>
2024-02-04 23:24:23 +0330

    filter by date and counted view added

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
+from datetime import datetime
 
 
 def blog_view(request):
-    posts = Post.objects.filter(status = 1)
+    posts = Post.objects.filter(published_date__lte = datetime.now(), status = 1)
     context = {"posts": posts}
     return render(request, 'blog/blog-home.html', context)
 
 
-def blog_single(request, pid):
-    post  = get_object_or_404(Post, pk = pid)
+def blog_single(request):
+    post  = get_object_or_404(Post)
+    post.counted_views = post.counted_views + 1
+    post.save()
     return render(request, 'blog/blog-single.html')
