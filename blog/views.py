8237007f7f99694b8c7f2f15d35ad2439d88b844
RezaKarami70 <reza.karami70@gmail.com>
2024-02-05 23:34:03 +0330

    ready for upload for second try

--- blog/views.py
+++ blog/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 from datetime import datetime
 
-
 def blog_view(request):
-    posts = Post.objects.filter(published_date__lte = datetime.now(), status = 1)
+    posts = Post.objects.filter(published_date__lte = datetime.now() , status = 1)
     context = {"posts": posts}
     return render(request, 'blog/blog-home.html', context)
 
 
-def blog_single(request):
-    post  = get_object_or_404(Post)
-    post.counted_views = post.counted_views + 1
+def blog_single(request, pid):
+    post = get_object_or_404(Post, pk=pid)
+    post.counted_views += 1
     post.save()
-    return render(request, 'blog/blog-single.html')
+    context = {"post": post}
+    return render(request, 'blog/blog-single.html',context)
