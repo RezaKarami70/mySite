eaca50c3266c0f67c8cfe98e85db1a2300aa56be
RezaKarami70 <reza.karami70@gmail.com>
2024-02-12 23:12:41 +0330

    ready for second exercise

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+    
+class Category(models.Model):
+        name = models.CharField(max_length=255)
+
+        def __str__(self):
+            return self.name
 # Create your models here.
 class Post(models.Model):
     image = models.ImageField(upload_to= 'blog/',default='blog/default.jpg')
@@ -8,7 +14,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     # tag
-    # category
+    category = models.ManyToManyField(Category)
     counted_views = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
     published_date = models.DateTimeField(null=True)
--- blog/views.py
+++ blog/views.py
@@ -3,16 +3,50 @@ from blog.models import Post
 from django.utils import timezone
 
 
+def pre(posts, target):
+    list = []
+    for post in posts:
+        list.append(post.id)
+    i = 0
+    for id in list:
+        if id == target.id:
+            break
+        i = i + 1
+    return list.__getitem__(i - 1)
+
+    
+
+
+
+def next(posts, target):
+    list = []
+    for post in posts:
+        list.append(post.id)
+    i = 0
+    for id in list:
+        if id == target.id:
+            break
+        i = i + 1
+
+    if i >= list.__len__()-1:
+        i = -1
+
+    return list.__getitem__(i + 1)
+
+
+
 def blog_view(request):
-    posts = Post.objects.filter(published_date__lte = timezone.now(), status = 1)
+    posts = Post.objects.filter(published_date__lte=timezone.now(), status=1)
     context = {"posts": posts}
     return render(request, 'blog/blog-home.html', context)
 
 
 def blog_single(request, pid):
-    posts = Post.objects.filter(published_date__lte = timezone.now(), status = 1)
+    posts = Post.objects.filter(published_date__lte=timezone.now(), status=1)
     post = get_object_or_404(posts, pk=pid)
+    p = posts.get(id = pre(posts, post))
+    n = posts.get(id = next(posts, post))
     post.counted_views += 1
     post.save()
-    context = {"post": post}
-    return render(request, 'blog/blog-single.html',context)
+    context = {"post": post, "pre": p, "next": n}
+    return render(request, 'blog/blog-single.html', context)
