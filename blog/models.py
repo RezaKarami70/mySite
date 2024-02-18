4c69953b424a779d16fa93bb21bd320022f74424
RezaKarami70 <reza.karami70@gmail.com>
2024-02-18 23:14:48 +0330

    ready for part 2 and 3

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,6 @@ class Post(models.Model):
     author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
-    # tag
     category = models.ManyToManyField(Category)
     counted_views = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
