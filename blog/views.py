05b861c74fae98528a82cfff323ac9d5a494fa81
RezaKarami70 <reza.karami70@gmail.com>
2024-02-26 22:08:07 +0330

    ready to upload for chapter7

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 from django.utils import timezone
+from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
 
 
 def pre(posts, target):
@@ -33,8 +34,20 @@ def next(posts, target):
     return list.__getitem__(i + 1)
 
 
-def blog_view(request):
+def blog_view(request, cat_name = None, author_username = None ):
     posts = Post.objects.filter(published_date__lte=timezone.now(), status=1)
+    if cat_name != None:
+        posts = posts.filter(category__name = cat_name)
+    if author_username != None:
+        posts = posts.filter(author__username = author_username)
+    posts = Paginator(posts,3)
+    try:
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
     context = {"posts": posts}
     return render(request, 'blog/blog-home.html', context)
 
@@ -48,3 +61,11 @@ def blog_single(request, pid):
     post.save()
     context = {"post": post, "pre": p, "next": n}
     return render(request, 'blog/blog-single.html', context)
+
+def blog_search(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now(), status=1)
+    if request.method == 'GET':
+        posts = posts.filter(content__contains = request.GET.get('s'))
+    context = {"posts": posts}
+    return render(request, 'blog/blog-home.html', context)
+
--- website/views.py
+++ website/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from blog.models import Post
+from django.utils import timezone
 
 # Create your views here.
 from django.http import HttpResponse, JsonResponse
 def index_view(request):
-    return render(request, 'website/index.html')
+    posts = Post.objects.filter(published_date__lte=timezone.now(), status=1).order_by("published_date").reverse()[:6]
+    context = {'posts': posts}
+    return render(request, 'website/index.html',context)
 
 def about_view(request):
     return render(request, 'website/about.html')
